8eb853b2ffc01f585fe8ec3c40fb46f09131347a
Aniurm <534147085@qq.com>
2024-05-26 15:26:38 +0800

    style: HTML

--- detect_video.py
+++ detect_video.py
@@ -20,9 +20,9 @@ Header = ["<h1 style='font-family:Montserrat'>My First Heading</h1>\n"]
 Short_Paragraph = ["<p style='font-family:Montserrat'>My first paragraph - This is a really cool way to quickly design and build websites...Don't you think?</p>\n"]
 Long_Paragraph = ["<p style='font-family:Montserrat'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n"]
 Button = ["<button type='button' style='font-family:Montserrat'>Click Me!</button>\n"]
-Banner = ["<img src='download.png' alt='Trulli' width='1200' height='224'>\n"]
+Banner = ["<img src='download.png' alt='Trulli' width='626' height='322'>\n"]
 Form = ["<h1  style='font-family:Montserrat'>Sign Up</h1><form action='/action_page.php'> <label for='fname' style='font-family:Montserrat'>First name: </label><input type='text' id='fname' name='fname'><br><br> <label for='lname'  style='font-family:Montserrat'>Last name: </label><input type='text' id='lname' name='lname'><br><br> <input type='submit' value='Submit' style='font-family:Montserrat'></form> "]
-Logo = ["<img src='Logo.png' alt='Logo' width='550' height='124'>\n"]
+Logo = ["<img src='Logo.png' alt='Logo' width='446' height='452'>\n"]
 
 count_threshold = 3
 
